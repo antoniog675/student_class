5686c4a0458ced5266d7ff1c9765030aeacc863a
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-07-05 16:13:14 +0000

    Added class and method testing for student email and name

--- test_student.py
+++ test_student.py
@@ -3,9 +3,32 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+    def setUp(self):
+        print('setUp')
+        self.student = Student('John', 'Doe')
+
+    def tearDown(self):
+        print('tearDown')
+
     def test_full_name(self):
-        student = Student('John', 'Doe')
-        self.assertEqual(student.full_name, 'John Doe')
+        print('test_full_name')
+        self.assertEqual(self.student.full_name, 'John Doe')
+
+    def test_alert_santa(self):
+        print('test_alert_santa')
+        self.student.alert_santa()
+    
+    def test_email_address(self):
+        print('test_email_address')
+        self.assertEqual(self.student.email_address, 'john.doe@email.com')
 
 if __name__ == '__main__':
     unittest.main()
